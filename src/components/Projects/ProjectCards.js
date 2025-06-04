46c61e0cfb9013ac7365c1e01892864ce5e361d9
anjali-94 <anjalishr987@gmail.com>
2025-06-04 16:32:22 +0530

    fifth commit

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,94 +1,94 @@
-// import React from "react";
-// import Card from "react-bootstrap/Card";
-// import Button from "react-bootstrap/Button";
-// import { CgWebsite } from "react-icons/cg";
-// import { BsGithub } from "react-icons/bs";
-
-// function ProjectCards(props) {
-//   return (
-//     <Card className="project-card-view">
-//       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-//       <Card.Body>
-//         <Card.Title>{props.title}</Card.Title>
-//         <Card.Text style={{ textAlign: "justify" }}>
-//           {props.description}
-//         </Card.Text>
-//         <Button variant="primary" href={props.ghLink} target="_blank">
-//           <BsGithub /> &nbsp;
-//           {props.isBlog ? "Blog" : "GitHub"}
-//         </Button>
-//         {"\n"}
-//         {"\n"}
-
-//         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-//         {!props.isBlog && props.demoLink && (
-//           <Button
-//             variant="primary"
-//             href={props.demoLink}
-//             target="_blank"
-//             style={{ marginLeft: "10px" }}
-//           >
-//             <CgWebsite /> &nbsp;
-//             {"Demo"}
-//           </Button>
-//         )}
-//       </Card.Body>
-//     </Card>
-//   );
-// }
-// export default ProjectCards;
-
-
-
-
-
-
-
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-import Tilt from "react-parallax-tilt"; // 
 
 function ProjectCards(props) {
   return (
-    <Tilt
-      tiltMaxAngleX={10}
-      tiltMaxAngleY={10}
-      glareEnable={true}
-      glareMaxOpacity={0.2}
-      scale={1.02}
-      transitionSpeed={250}
-    >
-      <Card className="project-card-view">
-        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text style={{ textAlign: "justify" }}>
-            {props.description}
-          </Card.Text>
-          <Button variant="primary" href={props.ghLink} target="_blank">
-            <BsGithub /> &nbsp;
-            {props.isBlog ? "Blog" : "GitHub"}
-          </Button>
+    <Card className="project-card-view">
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text style={{ textAlign: "justify" }}>
+          {props.description}
+        </Card.Text>
+        <Button variant="primary" href={props.ghLink} target="_blank">
+          <BsGithub /> &nbsp;
+          {props.isBlog ? "Blog" : "GitHub"}
+        </Button>
+        {"\n"}
+        {"\n"}
 
-          {!props.isBlog && props.demoLink && (
-            <Button
-              variant="primary"
-              href={props.demoLink}
-              target="_blank"
-              style={{ marginLeft: "10px" }}
-            >
-              <CgWebsite /> &nbsp;
-              {"Demo"}
-            </Button>
-          )}
-        </Card.Body>
-      </Card>
-    </Tilt>
+        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+
+        {!props.isBlog && props.demoLink && (
+          <Button
+            variant="primary"
+            href={props.demoLink}
+            target="_blank"
+            style={{ marginLeft: "10px" }}
+          >
+            <CgWebsite /> &nbsp;
+            {"Demo"}
+          </Button>
+        )}
+      </Card.Body>
+    </Card>
   );
 }
-
 export default ProjectCards;
+
+
+
+
+
+
+
+// import React from "react";
+// import Card from "react-bootstrap/Card";
+// import Button from "react-bootstrap/Button";
+// import { CgWebsite } from "react-icons/cg";
+// import { BsGithub } from "react-icons/bs";
+// import Tilt from "react-parallax-tilt"; // 
+
+// function ProjectCards(props) {
+//   return (
+//     <Tilt
+//       tiltMaxAngleX={10}
+//       tiltMaxAngleY={10}
+//       glareEnable={true}
+//       glareMaxOpacity={0.2}
+//       scale={1.02}
+//       transitionSpeed={250}
+//     >
+//       <Card className="project-card-view">
+//         <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+//         <Card.Body>
+//           <Card.Title>{props.title}</Card.Title>
+//           <Card.Text style={{ textAlign: "justify" }}>
+//             {props.description}
+//           </Card.Text>
+//           <Button variant="primary" href={props.ghLink} target="_blank">
+//             <BsGithub /> &nbsp;
+//             {props.isBlog ? "Blog" : "GitHub"}
+//           </Button>
+
+//           {!props.isBlog && props.demoLink && (
+//             <Button
+//               variant="primary"
+//               href={props.demoLink}
+//               target="_blank"
+//               style={{ marginLeft: "10px" }}
+//             >
+//               <CgWebsite /> &nbsp;
+//               {"Demo"}
+//             </Button>
+//           )}
+//         </Card.Body>
+//       </Card>
+//     </Tilt>
+//   );
+// }
+
+// export default ProjectCards;
